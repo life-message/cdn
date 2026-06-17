a1d60cee7b7ad8266ea2feef710b4aa3a38543e5
Дмитрий <64151271+falbue@users.noreply.github.com>
2026-06-17 14:23:55 +0500

    А почему то не было обновлений

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
+import './pages/menu.js';
+import './pages/templates.js';
 import notification from './notification.js';
-import './menu.js';
+import { initNavigation } from './ui/header.js';
+import './ui/color.js';
+import './utils/field-sync.js.js';
 
 globalThis.notification = notification;
+
+document.addEventListener('DOMContentLoaded', () => {
+    initNavigation();
+});
 
 export { notification };
 export default notification;
