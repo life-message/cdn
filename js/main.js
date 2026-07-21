17660fd4cf8581842b749941242ffba145761e64
falbue <cyansair05@gmail.com>
2026-07-21 19:33:12 +0500

    Скрипт, для вставки svg

--- js/main.js
+++ js/main.js
@@ -4,11 +4,12 @@ import notification from "./notification.js";
 import { initNavigation } from "./ui/header.js";
 import "./ui/color.js";
 import "./utils/localstorage.js";
+import "./utils/svg.js";
 
 globalThis.notification = notification;
 
 document.addEventListener("DOMContentLoaded", () => {
-    initNavigation();
+  initNavigation();
 });
 
 export { notification };
---
+++ js/utils/svg.js
@@ -0,0 +1,33 @@
+import { spa } from "./spa.js"
+
+function replaceSvg() {
+  const images = document.querySelectorAll('img[alt*="svg"]');
+
+  for (const img of images) {
+    try {
+      const response = fetch(img.src);
+
+      if (!response.ok) throw new Error('Ошибка загрузки файла');
+
+      const svgText = response.text();
+
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(svgText, 'image/svg+xml');
+      const svgElement = doc.querySelector('svg');
+
+      if (svgElement) {
+        if (img.className) svgElement.setAttribute('class', img.className);
+        if (img.id) svgElement.setAttribute('id', img.id);
+
+        if (img.width) svgElement.setAttribute('width', img.width);
+        if (img.height) svgElement.setAttribute('height', img.height);
+
+        img.replaceWith(svgElement);
+      }
+    } catch (error) {
+      console.error(`Не удалось заменить иконку ${img.src}:`, error);
+    }
+  }
+}
+
+spa(replaceSvg);
