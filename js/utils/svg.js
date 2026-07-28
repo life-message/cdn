e6a87e45460ef6ba248d6d38502944bf58e01950
falbue <cyansair05@gmail.com>
2026-07-28 14:46:52 +0500

    Исправлена работа svg скрипта

--- js/utils/svg.js
+++ js/utils/svg.js
@@ -1,15 +1,30 @@
 import { SPA } from "./spa.js"
 
+const svgCache = new Map();
+const processingSet = new Set();
+
 async function replaceSvg() {
   const images = document.querySelectorAll('img[alt*="svg"]');
 
   for (const img of images) {
+    if (img.dataset.svgReplaced === 'true' || processingSet.has(img.src)) {
+      continue;
+    }
+
+    processingSet.add(img.src);
+    img.dataset.svgReplaced = 'true';
+
     try {
-      const response = await fetch(img.src);
+      let svgText;
 
-      if (!response.ok) throw new Error('Ошибка загрузки файла');
-
-      const svgText = await response.text();
+      if (svgCache.has(img.src)) {
+        svgText = svgCache.get(img.src);
+      } else {
+        const response = await fetch(img.src);
+        if (!response.ok) throw new Error('Ошибка загрузки файла');
+        svgText = await response.text();
+        svgCache.set(img.src, svgText);
+      }
 
       const parser = new DOMParser();
       const doc = parser.parseFromString(svgText, 'image/svg+xml');
@@ -18,7 +33,6 @@ async function replaceSvg() {
       if (svgElement) {
         if (img.className) svgElement.setAttribute('class', img.className);
         if (img.id) svgElement.setAttribute('id', img.id);
-
         if (img.width) svgElement.setAttribute('width', img.width);
         if (img.height) svgElement.setAttribute('height', img.height);
 
@@ -26,6 +40,9 @@ async function replaceSvg() {
       }
     } catch (error) {
       console.error(`Не удалось заменить иконку ${img.src}:`, error);
+      img.removeAttribute('data-svg-replaced');
+    } finally {
+      processingSet.delete(img.src);
     }
   }
 }
